760cbf78bfc568c441dc378461acccee91d42ff4
Diego Ferreira <33259326+di-ferreira@users.noreply.github.com>
2021-05-07 13:08:32 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,13 +1,31 @@
 import ImgDsDark from 'assets/img/ds_dark.svg';
+import { Link } from 'react-router-dom';
 function NavBar() {
   return (
-    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-      <div className="container">
-        <nav className="my-2 my-md-0 mr-md-3">
-          <img src={ImgDsDark} alt="DevSuperior" width="120" />
-        </nav>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link to="/" className="navbar-brand">
+        <img src={ImgDsDark} alt="DevSuperior" width="120" />
+      </Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+        </ul>
       </div>
-    </div>);
+    </nav>
+  );
 }
 
 export default NavBar;
